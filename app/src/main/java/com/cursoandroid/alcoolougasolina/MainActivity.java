b3a0bb3d0227d6b6ce3a5a62a86039212c468689
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-04 16:37:13 -0300

    Validação dos campos digitados. Conversão dos valores, cálculo para a comparação e exibição do resultado na tela.

--- app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
@@ -3,12 +3,66 @@ package com.cursoandroid.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Recuperar ID's
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
     }
+
+    public void calcularPreco(View view){
+
+        //Recuperar os valores digitados
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        //Validar campos digitados
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+
+        if(camposValidados){
+
+            //Converter String para Number
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+            Double resultado = valorAlcool / valorGasolina;
+
+            //Calcular preços
+            if( resultado >= 0.7){
+                textResultado.setText("É melhor abastecer com Gasolina");
+            }else{
+                textResultado.setText("É melhor abastecer com Álcool");
+            }
+        }else{
+            textResultado.setText("Por favor, digite todos os preços");
+        }
+
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina){
+
+        Boolean camposValidados = true;
+
+        //Verificar campos vazios
+        if(pAlcool == null || pAlcool.equals("")){
+            camposValidados = false;
+        }else if(pGasolina == null || pGasolina.equals("")){
+            camposValidados = false;
+         }
+        return camposValidados;
+    }
+
 }
